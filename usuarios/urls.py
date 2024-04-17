e4a62957d986eb4feef7e220397b6eb1358d7fc5
Felipe Lopes <felipe10loppess@gmail.com>
2024-04-17 10:31:33 -0300

    Criando pastas templates e arquivos cadastro.html e base.html, além da configuração em Templates no Settings

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('cadastro/', views.cadastro, name="cadastro")
-]
+urlpatterns = [path("cadastro/", views.cadastro, name="cadastro")]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def cadastro(request):
-    print(request.META)
-    return HttpResponse('Olá mundo!')
+    return render(request, "cadastro.html")
