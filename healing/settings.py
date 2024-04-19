a2b374c666dd49f2c2edc422b7c21723d7c7017c
Felipe Lopes <felipe10loppess@gmail.com>
2024-04-19 15:34:31 -0300

    adicionando listagem de médicos

--- healing/settings.py
+++ healing/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "usuarios",
     "medico",
+    "paciente",
+
 ]
 
 MIDDLEWARE = [
---
+++ paciente/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from medico.models import DadosMedico
+# Create your views here.
+def home(request):
+    if request.method == "GET":
+        medicos =  DadosMedico.objects.all()
+        return render(request, 'home.html', {'medicos': medicos})
